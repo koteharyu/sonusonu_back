95bee1d49f02ab221f28d6d467b13087edf738b2
haryu <hryau6@gmail.com>
2021-10-13 14:08:53 +0900

    add: display author_image

--- app/controllers/api/v1/gournals_controller.rb
+++ app/controllers/api/v1/gournals_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::GournalsController < ApplicationController
 
   def index
     gournals = Gournal.order(created_at: :desc)
-    render json: gournals, methods: [:image_url]
+    render json: gournals, methods: [:image_url, :author_image_url]
   end
 
   def create
     gournal = Gournal.new(gournal_params)
     if gournal.save
-      render json: gournal, methods: [:image_url]
+      render json: gournal, methods: [:image_url, :author_image_url]
     else
       render json: gournal.errors
     end
@@ -17,6 +17,6 @@ class Api::V1::GournalsController < ApplicationController
   private
 
   def gournal_params
-    params.require(:gournal).permit(:title, :content, :image)
+    params.require(:gournal).permit(:title, :content, :image, :author_image)
   end
 end
--- app/models/gournal.rb
+++ app/models/gournal.rb
@@ -5,8 +5,13 @@ class Gournal < ApplicationRecord
   validates :content, presence: true
 
   has_one_attached :image
+  has_one_attached :author_image
 
   def image_url
     image.attached? ? url_for(image) : nil
+  end
+
+  def author_image_url
+    author_image.attached? ? url_for(author_image) : nil
   end
 end
