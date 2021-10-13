46c2193c1d0bcb57290bb832648f76fceb7895f7
haryu <hryau6@gmail.com>
2021-10-13 11:02:13 +0900

    add: basic gorunals controller

---
+++ app/controllers/api/v1/gournals_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::GournalsController < ApplicationController
+
+  def index
+    gournals = Gournal.order(created_at: :desc)
+    render json: gournals
+  end
+
+  def create
+    gournal = Gournal.new(gournal_params)
+    if gournal.save
+      render json: gournal
+    else
+      render json: gournal.errors
+    end
+  end
+
+  private
+
+  def gournal_params
+    params.require(:gournal).permit(:title, :content)
+  end
+end
